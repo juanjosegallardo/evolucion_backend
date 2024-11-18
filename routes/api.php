20c30b6b9eda773c6e72a0a4e2a58c4dda7ec83a
unknown <isc.juan.gallard@gmail.com>
2024-11-18 00:34:52 -0600

    correcciones

--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource("/almacenes", AlmacenController::class );
-Route::resource("/articulos", ArticuloController::class );
-Route::resource("/cargas", CargaController::class );
-Route::resource("/vendedores", VendedorController::class );
-Route::resource("/traspasos", TraspasoController::class );
-Route::resource("/ventas", VentaController::class );
-Route::resource("/tipos_articulos", TipoArticuloController::class );
+
 
 Route::resource("/cargas/{id}/articulos", CargaArticuloController::class);
 Route::delete("/carga_articulo/{id}", [CargaArticuloController::class,"destroy"]);
@@ -33,3 +27,11 @@ Route::delete("/traspaso_articulo/{id}", [TraspasoArticuloController::class,"des
 
 Route::resource("/ventas/{id}/articulos", VentaArticuloController::class);
 Route::delete("/venta_articulo/{id}", [VentaArticuloController::class,"destroy"]);
+
+Route::resource("/almacenes", AlmacenController::class );
+Route::resource("/articulos", ArticuloController::class );
+Route::resource("/cargas", CargaController::class );
+Route::resource("/vendedores", VendedorController::class );
+Route::resource("/traspasos", TraspasoController::class );
+Route::resource("/ventas", VentaController::class );
+Route::resource("/tipos_articulos", TipoArticuloController::class );
