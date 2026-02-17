4d1303c3ad2e0065f8426d05813d9cc6cf0d45f1
unknown <isc.juan.gallard@gmail.com>
2026-02-17 17:12:35 -0600

    correcciones

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -19,7 +19,8 @@ class UsuarioSeeder extends Seeder
             ["id"=>3, "nombre"=>"Carlos Garcia Vega", "usuario"=>"carlos", "password"=>bcrypt("car7895455"), "role"=>"user"],
             ["id"=>4, "nombre"=>"Francisco Agustin Magaña Duarte", "usuario"=>"agustin", "password"=>bcrypt("agus545465"), "role"=>"user"],
             ["id"=>5, "nombre"=>"Jesus Santos", "usuario"=>"santos", "password"=>bcrypt("santos456545"), "role"=>"user"],
-            ["id"=>6, "nombre"=>"Camilo Gonzalez", "usuario"=>"camilo", "password"=>bcrypt("camilo78978"), "role"=>"user"]
+            ["id"=>6, "nombre"=>"Camilo Gonzalez", "usuario"=>"camilo", "password"=>bcrypt("camilo78978"), "role"=>"user"],
+            ["id"=>7, "nombre"=>"Ramon Santiago", "usuario"=>"camilo", "password"=>bcrypt("ramon324235"), "role"=>"user"]
 
         ];
 
