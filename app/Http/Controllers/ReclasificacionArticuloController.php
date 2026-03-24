3294af4d6738017574eedde3ba19d99408b0e2ed
unknown <isc.juan.gallard@gmail.com>
2026-03-24 17:14:47 -0600

    correcciones

--- app/Http/Controllers/ReclasificacionArticuloController.php
+++ app/Http/Controllers/ReclasificacionArticuloController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Reclasificacion;
 use App\Services\ReclasificacionArticuloService;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
