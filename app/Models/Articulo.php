7eb8189a31d97f443733797d033579309282e255
unknown <isc.juan.gallard@gmail.com>
2026-03-24 18:09:35 -0600

    correcciones

--- app/Models/Articulo.php
+++ app/Models/Articulo.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use App\Models\TipoArticulo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Articulo extends Model
 {
     use SoftDeletes;
@@ -17,18 +17,17 @@ class Articulo extends Model
         return $this->belongsTo(TipoArticulo::class);
     }
 
-    public function scopeConExistenciaEn(Builder $query, $almacenId)
+    public function scopeConExistenciaEn(\Illuminate\Database\Eloquent\Builder $query, $almacenId)
     {
         return $query
             ->select(
-                "articulos.*",
-                DB::raw("COALESCE(pivot.cantidad, 0) as cantidad"),
-                DB::raw("COALESCE(pivot.cantidad_defectuosos, 0) as cantidad_defectuosos"),
-                "pivot.id as pivot_id"
+                'articulos.*',
+                DB::raw('COALESCE(pivot.cantidad, 0) as cantidad'),
+                DB::raw('COALESCE(pivot.cantidad_defectuosos, 0) as cantidad_defectuosos')
             )
-            ->leftJoin("almacen_articulo as pivot", function ($join) use ($almacenId) {
-                $join->on("articulos.id", "=", "pivot.articulo_id")
-                    ->where("pivot.almacen_id", $almacenId);
+            ->leftJoin('almacen_articulo as pivot', function ($join) use ($almacenId) {
+                $join->on('articulos.id', '=', 'pivot.articulo_id')
+                    ->where('pivot.almacen_id', $almacenId);
             });
     }
 }
